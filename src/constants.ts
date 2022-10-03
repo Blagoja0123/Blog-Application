7e2dce601cb2f315f53bce8ad5bf6b7fb7dfe2a3
Blagoja0123 <dzorlevblagoja@yahoo.com>
2022-10-03 11:22:10 +0200

    more fixes

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const baseUrl = process.env.VERCEL_URL
-    ? 'https://test-next-c1as4xtmq-blagoja0123.vercel.app/'
+    ? 'https://test-next-red-ten.vercel.app/'
     : 'http://localhost:3000/'
 
 export const url = `${baseUrl}/api/trpc`
