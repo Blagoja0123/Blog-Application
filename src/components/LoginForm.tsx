c1c278ce0762556fbf85e7491d0cf14716887c8c
Blagoja0123 <dzorlevblagoja@yahoo.com>
2022-10-11 22:07:49 +0200

    prevod

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,75 +1,75 @@
 import Link from "next/link"
 import { useRouter } from "next/router";
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { CreateUserInput } from "../backend/schema/user.schema";
 import { trpc } from "../utils/trpc";
 import { useState } from "react";
 import { redirect } from "next/dist/server/api-utils";
 
-const VerifyToken = ({hash}:{hash: string})=>{
+const VerifyToken = ({ hash }: { hash: string }) => {
     const router = useRouter();
-        const {data, isLoading} = trpc.useQuery(['users.verify-otp', {
-            hash,
-        }])
+    const { data, isLoading } = trpc.useQuery(['users.verify-otp', {
+        hash,
+    }])
 
-        if(isLoading){
-            return <p>Verifying...</p>
-        }
+    if (isLoading) {
+        return <p>Verifying...</p>
+    }
 
-        router.push(data?.redirect.includes('login') ? '/': data?.redirect || '/')
+    router.push(data?.redirect.includes('login') ? '/' : data?.redirect || '/')
 
-        return <p>Redirecting...</p>
+    return <p>Redirecting...</p>
 }
 
-export const LoginForm = () =>{
+export const LoginForm = () => {
     const [success, setSuccess] = useState(false);
     const [link, setLink] = useState('');
-    const {handleSubmit, register} = useForm<CreateUserInput>();
+    const { handleSubmit, register } = useForm<CreateUserInput>();
     const router = useRouter();
 
-        
 
-    const {mutate, data, error} = trpc.useMutation(['users.login-otp'], {
-        onSuccess: ()=>{
+
+    const { mutate, data, error } = trpc.useMutation(['users.login-otp'], {
+        onSuccess: () => {
             setSuccess(true);
         }
     })
 
-    function onSubmit (values: CreateUserInput){
-        mutate({...values, redirect: router.asPath})
+    function onSubmit(values: CreateUserInput) {
+        mutate({ ...values, redirect: router.asPath })
     }
 
     const hash = router.asPath.split('#token=')[1]
 
     if (hash) {
-        return <VerifyToken hash={hash}/>
+        return <VerifyToken hash={hash} />
     }
 
 
     return (
-    <>
-        <div className=" flex items-center justify-center">
-            <div className=" bg-zinc-700 p-12 rounded-xl mt-36 w-fit h-fit items-center content-center text-center shadow-lg">
-                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
-                    {error && error.message}
-                    {success && <Link href={data?.test || ''}>check email</Link>}
-                <h1 className=" text-center font-bold text-3xl text-white">Login</h1>
-                <br/>
-                <div className=" justify-center">
-                    <div className=" space-y-3">
-                        <input type='text' placeholder="email" {...register('email')} className=" bg-white placeholder-black rounded-md pr-16 pl-2 py-4 text-left text-black w-full"/>
-                        <br/>
-                        <input type='text' placeholder="password" {...register('password')} className=" bg-white placeholder-black rounded-md pr-16 pl-2 py-4 text-left text-black w-full"/>
-                        <br/>
-                            <button type='submit' className=" bg-zinc-800 rounded-md px-40 py-4 flex item text-xl w-full hover:bg-zinc-900 text-white">Login</button>
-                        <br/>
-                    </div>
+        <>
+            <div className=" flex items-center justify-center">
+                <div className=" bg-zinc-700 p-12 rounded-xl mt-36 w-fit h-fit items-center content-center text-center shadow-lg">
+                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+                        {error && error.message}
+                        {success && <Link href={data?.test || ''}>check email</Link>}
+                        <h1 className=" text-center font-bold text-3xl text-white">Најава</h1>
+                        <br />
+                        <div className=" justify-center">
+                            <div className=" space-y-3">
+                                <input type='text' placeholder="email" {...register('email')} className=" bg-white placeholder-black rounded-md pr-16 pl-2 py-4 text-left text-black w-full" />
+                                <br />
+                                <input type='text' placeholder="password" {...register('password')} className=" bg-white placeholder-black rounded-md pr-16 pl-2 py-4 text-left text-black w-full" />
+                                <br />
+                                <button type='submit' className=" bg-zinc-800 rounded-md px-40 py-4 flex item text-xl w-full hover:bg-zinc-900 text-white">Најава</button>
+                                <br />
+                            </div>
+                        </div>
+                    </form>
+                    <span className=" text-lg text-white">Нови на оваа страница? <span className=" text-sky-700"><Link href="/register">Регистрирајте се</Link></span></span>
                 </div>
-                </form>
-                <span className=" text-lg text-white">New here? <span className=" text-sky-700"><Link href="/register">Register</Link></span></span>
             </div>
-        </div>
-    </>
+        </>
     )
 }
 
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,52 +1,52 @@
 import Link from "next/link"
 import { useRouter } from "next/router";
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { CreateUserInput } from "../backend/schema/user.schema";
 import { NavBar } from "../components/NavBar";
 import { trpc } from "../utils/trpc";
-export const Register = () =>{
-    const {handleSubmit, register} = useForm<CreateUserInput>();
+export const Register = () => {
+    const { handleSubmit, register } = useForm<CreateUserInput>();
     const router = useRouter();
 
-    const {mutate, error} = trpc.useMutation((['users.register']), {
-        onSuccess: () =>{
+    const { mutate, error } = trpc.useMutation((['users.register']), {
+        onSuccess: () => {
             router.push('/login')
         },
     })
 
-    function onSubmit (values: CreateUserInput){
+    function onSubmit(values: CreateUserInput) {
         mutate(values)
     }
-    
-    
+
+
 
     return (
-    <>
-    <body>
-        <NavBar/>
-        <div className=" flex items-center justify-center align-middle pt-16">
-            <div className=" bg-zinc-700 p-12 rounded-xl mb-36 pt-16 w-fit h-fit flex-row items-center content-center text-center">
-                <form onSubmit={handleSubmit(onSubmit)}>{error && error.message}
-                    <h1 className=" text-center font-bold text-3xl text-white">Register</h1>
-                    <br/>
-                    <div className=" container justify-center ">
-                        <div className=" flex-row space-y-3">
-                            <input type='text' placeholder="username" {...register('username')} className=" bg-white placeholder-black rounded-md pr-16 pl-2 py-4 text-left text-black w-full"/>
-                            <br/>
-                            <input type='text' placeholder="email" {...register('email')} className=" bg-white placeholder-black rounded-md pr-16 py-4 pl-2 text-left text-black w-full"/>
-                            <br/>
-                            <input type='text' placeholder="password" {...register('password')} className=" bg-white placeholder-black rounded-md pr-16 pl-2 py-4 text-left text-black w-full"/>
-                            <br/>
-                            <button type='submit' className=" bg-zinc-800 rounded-md px-40 py-4 flex item text-xl w-full hover:bg-zinc-900 text-white">Register</button>
-                            <br/>
-                        </div>
+        <>
+            <body>
+                <NavBar />
+                <div className=" flex items-center justify-center align-middle pt-16">
+                    <div className=" bg-zinc-700 p-12 rounded-xl mb-36 pt-16 w-fit h-fit flex-row items-center content-center text-center">
+                        <form onSubmit={handleSubmit(onSubmit)}>{error && error.message}
+                            <h1 className=" text-center font-bold text-3xl text-white">Регистрирација</h1>
+                            <br />
+                            <div className=" container justify-center ">
+                                <div className=" flex-row space-y-3">
+                                    <input type='text' placeholder="username" {...register('username')} className=" bg-white placeholder-black rounded-md pr-16 pl-2 py-4 text-left text-black w-full" />
+                                    <br />
+                                    <input type='text' placeholder="email" {...register('email')} className=" bg-white placeholder-black rounded-md pr-16 py-4 pl-2 text-left text-black w-full" />
+                                    <br />
+                                    <input type='text' placeholder="password" {...register('password')} className=" bg-white placeholder-black rounded-md pr-16 pl-2 py-4 text-left text-black w-full" />
+                                    <br />
+                                    <button type='submit' className=" bg-zinc-800 rounded-md px-40 py-4 flex item text-xl w-full hover:bg-zinc-900 text-white">Регистрација</button>
+                                    <br />
+                                </div>
+                            </div>
+                        </form>
+                        <span className=" text-lg text-white">Веќе имате профил? <span className=" text-sky-700"><Link href='/login'>Најавете се</Link></span></span>
                     </div>
-                </form>
-                <span className=" text-lg text-white">Already have an account? <span className=" text-sky-700"><Link href='/login'>Login</Link></span></span>
-            </div>
-        </div>
-    </body>    
-    </>
+                </div>
+            </body>
+        </>
     )
 }
 
